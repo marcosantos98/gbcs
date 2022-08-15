97c1ebb82ae8bedeb6e7f900aea91dacf1397e35
mscode98 <workmarco98@gmail.com>
2022-08-15 12:43:36 +0100

    A8_R, A16_R, D16_R, R_D8, D8, R_A8, HLSPR

--- gb/AddressDataHandlers.cs
+++ gb/AddressDataHandlers.cs
@@ -9,8 +9,20 @@ namespace GBCS.GB
             Handlers.Add(AddressMode.IMP, cpu => { });
             Handlers.Add(AddressMode.R, cpu => cpu.AddressData = cpu.GetRegister(cpu.Inst.RegOne));
             Handlers.Add(AddressMode.R_R, cpu => cpu.AddressData = cpu.GetRegister(cpu.Inst.RegTwo));
-            Handlers.Add(AddressMode.D16, D16);
-            Handlers.Add(AddressMode.R_D16, D16);
+            Handlers.Add(AddressMode.A8_R, cpu =>
+            {
+                cpu.AddressData = cpu.Mem.Read(cpu.Pc++);
+                cpu.PcIsMemDest = true;
+                //fixme 22/08/14: Cycles
+            });
+            Handlers.Add(AddressMode.D16, RD16_D16);
+            Handlers.Add(AddressMode.R_D16, RD16_D16);
+            Handlers.Add(AddressMode.A16_R, A16R_D16R);
+            Handlers.Add(AddressMode.D16_R, A16R_D16R);
+            Handlers.Add(AddressMode.R_D8, RD8_D8_RA8_HLSPR);
+            Handlers.Add(AddressMode.D8, RD8_D8_RA8_HLSPR);
+            Handlers.Add(AddressMode.R_A8, RD8_D8_RA8_HLSPR);
+            Handlers.Add(AddressMode.HL_SPR, RD8_D8_RA8_HLSPR);
         }
 
         public static Action<CPU> Get(AddressMode mode)
@@ -18,7 +30,7 @@ namespace GBCS.GB
             return Handlers[mode];
         }
 
-        private static void D16(CPU cpu)
+        private static void RD16_D16(CPU cpu)
         {
             byte lo = cpu.Mem.Read(cpu.Pc);
             //fixme 22/08/14: Cycles
@@ -26,6 +38,25 @@ namespace GBCS.GB
             //fixme 22/08/14: Cycles
             cpu.AddressData = (ushort)(lo | (hi << 8));
             cpu.Pc += 2;
+        }
+
+        private static void A16R_D16R(CPU cpu)
+        {
+            byte lo = cpu.Mem.Read(cpu.Pc);
+            //fixme 22/08/14: Cycles
+            byte hi = cpu.Mem.Read((ushort)(cpu.Pc + 1));
+            //fixme 22/08/14: Cycles
+            cpu.MemDest = (ushort)(lo | (hi << 8));
+            Logger.LogLn("\tLow 0x{0:X2}, Hi 0x{1:X2}, Val: 0x{2:X4}", lo, hi, cpu.MemDest);
+            cpu.PcIsMemDest = true;
+            cpu.Pc += 2;
+            cpu.AddressData = cpu.GetRegister(cpu.Inst.RegTwo);
+        }
+
+        private static void RD8_D8_RA8_HLSPR(CPU cpu)
+        {
+            cpu.AddressData = cpu.Mem.Read(cpu.Pc++);
+            //fixme 22/08/14: cycles
         }
     }
 }
--- gbtest/AdressHandlersTest.cs
+++ gbtest/AdressHandlersTest.cs
@@ -20,6 +20,35 @@ namespace GBCS.GBTest
             Assert.Equal(cpu.GetRegister(twoReg), cpu.AddressData);
         }
 
+        [Fact]
+        public static void A8_R()
+        {
+            CPU cpu = new();
+            AddressDataHandlers.Get(AddressMode.A8_R).Invoke(cpu);
+            Assert.True(cpu.PcIsMemDest);
+        }
+
+        [Fact]
+        public static void RD8_D8_RA8_HLSPR()
+        {
+            CPU cpu = new();
+            {
+                cpu.Pc = 0xC001;
+            }
+            cpu.Mem.Write(0xC001, 69);
+            AddressDataHandlers.Get(AddressMode.R_D8).Invoke(cpu);
+            Assert.Equal(69, cpu.AddressData);
+            cpu.Pc = 0xC001;
+            AddressDataHandlers.Get(AddressMode.D8).Invoke(cpu);
+            Assert.Equal(69, cpu.AddressData);
+            cpu.Pc = 0xC001;
+            AddressDataHandlers.Get(AddressMode.R_A8).Invoke(cpu);
+            Assert.Equal(69, cpu.AddressData);
+            cpu.Pc = 0xC001;
+            AddressDataHandlers.Get(AddressMode.HL_SPR).Invoke(cpu);
+            Assert.Equal(69, cpu.AddressData);
+        }
+
         [Theory]
         [InlineData(0x34, 0x35, 0x3534)]
         [InlineData(0xF0, 0x0F, 0x0FF0)]
@@ -36,6 +65,29 @@ namespace GBCS.GBTest
             cpu.Pc = 0xC000;
             AddressDataHandlers.Get(AddressMode.D16).Invoke(cpu);
             Assert.Equal(expected, cpu.AddressData);
+        }
+
+        [Theory]
+        [InlineData(0x01, 0x01, 0x0101, RegisterType.A, 0xA0)]
+        public static void A16R_D16R(byte one, byte two, ushort expected, RegisterType a, byte aVal)
+        {
+            CPU cpu = new()
+            {
+                Pc = 0xC000
+            };
+            cpu.Mem.Write(0xC000, one);
+            cpu.Mem.Write(0xC001, two);
+            cpu.SetRegister(a, aVal);
+            cpu.Inst = IBuilder.Type(InstructionType.NOP).Two(a).Ret();
+            AddressDataHandlers.Get(AddressMode.A16_R).Invoke(cpu);
+            Assert.Equal(expected, cpu.MemDest);
+            Assert.True(cpu.PcIsMemDest);
+            Assert.Equal(aVal, cpu.AddressData);
+            cpu.Pc = 0xC000;
+            AddressDataHandlers.Get(AddressMode.D16_R).Invoke(cpu);
+            Assert.Equal(expected, cpu.MemDest);
+            Assert.True(cpu.PcIsMemDest);
+            Assert.Equal(aVal, cpu.AddressData);
         }
     }
 }
